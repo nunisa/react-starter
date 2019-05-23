bb17e92e8aaf81843be4fb876670a77f904dfa95
Monjit Nunisa <monjitnunisa@Monjits-MacBook-Pro.local>
2019-05-23 08:01:41 +0530

    Jest testing framework configs added

---
+++ src/components/App/App.test.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import App from './';
+
+let wrapper;
+
+describe('App component', () => {
+    beforeEach(() => {
+        wrapper = shallow(<App />);
+    });
+
+    it('should be defined', () => {
+        expect(typeof App).toEqual('function');
+    });
+
+    it('should be rendered without error', () => {
+        expect(wrapper.length).toEqual(1);
+    });
+});
